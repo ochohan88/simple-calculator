441e2ccebd3cdd42ad2096bf53caf514aef27f62
ochohan88 <ochohan88@gmail.com>
2025-12-23 12:43:02 +0500

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function calculate() {
         display.value = "Error";
     }
 }
+document.addEventListener("keydown", function (event) {
+    if (
+        (event.key >= "0" && event.key <= "9") ||
+        event.key === "+" ||
+        event.key === "-" ||
+        event.key === "*" ||
+        event.key === "/" ||
+        event.key === "."
+    ) {
+        appendValue(event.key);
+    }
+
+    if (event.key === "Enter") {
+        calculate();
+    }
+
+    if (event.key === "Backspace") {
+        display.value = display.value.slice(0, -1);
+    }
+});
+
